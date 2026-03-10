2d99de0781813ce893ca199e2a5f075ed73b1764
Muhammad suhail <mhdsuhailmohaideen786@gmail.com>
2026-03-10 16:36:31 +0530

    ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import MenuPage from "./pages/MenuPage";
-import { useEffect } from "react"
+import { useEffect } from "react";
+
 function App() {
+
   useEffect(() => {
-    window.scrollTo(0, 1)
-  }, [])
+    const timer = setTimeout(() => {
+      window.scrollTo(0, 1);
+    }, 300);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <Routes>
       <Route path="/" element={<Navigate to="/menu/breakfast" />} />
